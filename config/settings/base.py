bc9b25e7f0caa1797c19a87a5b5d2a9b1a30ef4f
Ryszard Orlikowski <ryszardorlikowski93@gmail.com>
2021-03-20 16:49:57 +0100

    Add accounts app with main user model

--- config/settings/base.py
+++ config/settings/base.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
 ]
 
 COSTUME_APP = [
-
+    'accounts',
 ]
 
 INSTALLED_APPS += COSTUME_APP
@@ -112,3 +112,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# MAIN USER MODEL
+AUTH_USER_MODEL = 'accounts.User'
