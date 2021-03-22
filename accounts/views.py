e078cafcb8083171c982d7b9489dd9fe39f77d33
Ryszard Orlikowski <ryszardorlikowski93@gmail.com>
2021-03-22 11:36:10 +0100

    Add user profile endponint

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from accounts.serializers import UserSerializer
 
 
@@ -7,3 +8,12 @@ class CreateUserAPIView(CreateAPIView):
     """Register user API view"""
     model = get_user_model()
     serializer_class = UserSerializer
+
+
+class UserProfileAPIView(RetrieveAPIView):
+    """Return user profile"""
+    serializer_class = UserSerializer
+    permission_classes = (IsAuthenticated, )
+
+    def get_object(self):
+        return get_user_model().objects.get(pk=self.request.user.pk)
