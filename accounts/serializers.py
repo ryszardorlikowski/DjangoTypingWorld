6a2f50ba1a8d7599083143427791ee467c46d149
Ryszard Orlikowski <ryszardorlikowski93@gmail.com>
2021-03-22 14:37:53 +0100

    Add user create method  in serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,6 +6,10 @@ class UserSerializer(serializers.ModelSerializer):
     """Main user model serializer"""
     password = serializers.CharField(write_only=True)
 
+    def create(self, validated_data):
+        user = get_user_model().objects.create_user(**validated_data)
+        return user
+
     class Meta:
         model = get_user_model()
         fields = ['username', 'password', 'email']
